d503e90aba15767408e830c3ae62845ee2cbf5eb
Krasimir <krasi4367@gmail.com>
2024-11-25 15:45:05 +0100

    test

--- checkargs.sh
+++ checkargs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 EXPECTED_OUTPUT=$(echo -e "$2")
-PROGRAM_OUTPUT=$($1 2>&1)
+PROGRAM_OUTPUT=$($1 2>&1 | tr -d '\r')
 
 echo "Expected output: '$EXPECTED_OUTPUT'"
 echo "Program output: '$PROGRAM_OUTPUT'"
 
-if [ "$PROGRAM_OUTPUT" == "$EXPECTED_OUTPUT" ]; then
+if [ "$PROGRAM_OUTPUT" = "$EXPECTED_OUTPUT" ]; then
     echo "Test passed"
     exit 0
 else
